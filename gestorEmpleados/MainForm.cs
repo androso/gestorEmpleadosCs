518189ad20ce4c5aef93ec49ac6126f368e305c4
androso <androso421@gmail.com>
2024-08-24 20:59:27 -0600

    chore: initializing list box with employees

--- gestorEmpleados/MainForm.cs
+++ gestorEmpleados/MainForm.cs
@@ -1,3 +1,5 @@
+using EmployeeManagementLibrary.Models;
+
 namespace gestorEmpleados
 {
     public partial class MainForm : Form
@@ -5,6 +7,10 @@ namespace gestorEmpleados
         public MainForm()
         {
             InitializeComponent();
+            EmployeeManagement employeeManagement = new EmployeeManagement();
+            employeesListBox.Items.AddRange(employeeManagement.GetAllEmployees().ToArray());
+            employeesListBox.DisplayMember = "FullName";
+            employeesListBox.ValueMember = "Id";
         }
 
         private void newEmployeeButton_Click(object sender, EventArgs e)
