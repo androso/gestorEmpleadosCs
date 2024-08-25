61bfa07176da09b7bc07d9d65e632cbc1f60318f
androso <androso421@gmail.com>
2024-08-24 20:57:28 -0600

    chore: modify employee library so that it has a management and employee class

--- EmployeeManagementLibrary/Models/Employee.cs
+++ EmployeeManagementLibrary/Models/Employee.cs
@@ -6,8 +6,54 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagementLibrary.Models
 {
-    internal class Employee
+    public class Employee
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Seniority { get; set; }
+    }
 
+    public class EmployeeManagement
+    {
+        private List<Employee> employees;
+
+        public EmployeeManagement()
+        {
+            employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FullName = "John Viber",
+                    BirthDate = new DateTime(1990, 1, 1),
+                    Seniority = "Junior"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FullName = "Jane Doe",
+                    BirthDate = new DateTime(1985, 2, 2),
+                    Seniority = "Mid"
+                },
+                new Employee
+                {
+                    Id = 3,
+                    FullName = "Alice Smith",
+                    BirthDate = new DateTime(1980, 3, 3),
+                    Seniority = "Senior"
+                }
+            };
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
     }
 }
