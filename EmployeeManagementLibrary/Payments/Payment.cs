bde129658a5e6b9c4d82eba4bbedc19cb7ca4142
androso <androso421@gmail.com>
2024-08-24 22:30:27 -0600

    chore: adding custom event handler for payments

--- EmployeeManagementLibrary/Payments/Payment.cs
+++ EmployeeManagementLibrary/Payments/Payment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagementLibrary.Payments
 {
-    internal class Payment
+    public class PaymentEventArgs : EventArgs
     {
+        public Employee Employee { get; }
+        public decimal Amount { get; }
+
+        public PaymentEventArgs(Employee employee, decimal amount)
+        {
+            Employee = employee;
+            Amount = amount;
+        }
+    }
+
+    public class PaymentGenerator
+    {
+        public event EventHandler<PaymentEventArgs> PaymentGenerated;
+
+        public void GeneratePayment(Employee employee, decimal amount)
+        {
+            // Raise the event
+            OnPaymentGenerated(new PaymentEventArgs(employee, amount));
+
+            Console.WriteLine($"Payment of {amount:C} generated for {employee.FullName}");
+        }
+
+        protected virtual void OnPaymentGenerated(PaymentEventArgs e)
+        {
+            PaymentGenerated?.Invoke(this, e);
+        }
     }
 }
